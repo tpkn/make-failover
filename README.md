2d710b86acade22126b896e28eac632113dde594
tpkn <tpkn@users.noreply.github.com>
2018-09-19 22:24:01 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# make-failover
+# Make Failover   
 Making failover images with a blink of an eye
+
+
+A silent module based on [ImageMagick](https://imagemagick.org) for making failover images with specified properties.
+
+
+
+## API
+
+### MakeFailover(width, height, output_path[, options])
+
+### width   
+**Type**: _Number_  
+Image width, `> 0` and `<= 99999` pixels
+
+### height   
+**Type**: _Number_   
+Image height, `> 0` and `<= 99999` pixels
+
+
+### output_path  
+**Type**: _String_  
+Output file path with extension (`.jpg`, `.png`, `.gif`, `.svg`, ...)
+
+
+### options.bin
+**Type**: _String_    
+**Default**: `./bin/convert.exe`  
+Path to `converter.exe` file. This binary is not included, you can [download it from here](https://imagemagick.org/script/download.php). 
+
+
+
+### options.quality
+**Type**: _Number_  
+**Default**: `75`  
+
+
+### options.border_stroke
+**Type**: _Number_   
+**Default**: : `1`  
+Border stroke size
+
+
+### options.border_color
+**Type**: _String_   
+**Default**: : `#000000`  
+
+
+### options.bg_color
+**Type**: _String_   
+**Default**: : `#ffffff`  
+
+
+
+
+
+## Usage
+```javascript
+const path = require('path');
+const MakeFailover = require('make-failover');
+
+(async () => {
+
+   let width = 300;
+   let height = 250;
+   let output = path.join(__dirname, Date.now() + '.jpg')
+   
+   let failover = await MakeFailover(width, height, output, {
+      border_stroke: 20,
+      border_color: '#ff9900',
+      bg_color: '#00ffbb',
+      quality: 90
+   }); // => C:/make-failover/1537383269062.jpg
+
+})();
+```
+
+
---
+++ make-failover.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+/**
+ * @param  {Number} width
+ * @param  {Number} height
+ * @param  {String} output_path
+ * @param  {Object} options       { bin, quality, border_stroke, border_color, bg_color }
+ * @return {String}               path to the failover file | null
+ */
+function makeFailover(width, height, output_path, options = {}){
+   return new Promise(async (resolve, reject) => {
+      let cmd = [];
+      let failover_path;
+
+      // Set converter.exe
+      let bin = path.join('./bin/', 'convert.exe');
+      if(typeof options.bin === 'string' && fs.existsSync(options.bin)){
+         bin = options.bin;
+      }else if(!fs.existsSync(bin)){
+         throw new Error(`no convert.exe file`)
+      }
+
+      try {
+         // Check if the size is valid (0x0 - 99999x99999)
+         if(typeof width !== 'number' || width < 1 || width > 99999){
+            throw new Error(`no valid width`)
+         }
+         if(typeof height !== 'number' || height < 1 || height > 99999){
+            throw new Error(`no valid height`)
+         }
+
+         // Check if the output file path is set
+         if(typeof output_path !== 'string'){
+            throw new Error(`${output_path} path is not set`)
+         }
+
+         // Set image quality
+         let quality = 75;
+         if(typeof options.quality === 'number'){
+            quality = options.quality;
+         }
+
+         // Set border stroke size
+         let border_stroke = 1;
+         if(typeof options.border_stroke === 'number' && options.border_stroke >= 0){
+            border_stroke = options.border_stroke;
+         }
+         
+         // Add border
+         let border_color = '#000000';
+         if(typeof options.border_color === 'string' && /^#[a-f0-9]{6}$/i.test(options.border_color)){
+            border_color = options.border_color;
+         }
+
+         // Set background color
+         let bg_color = '#ffffff';
+         if(typeof options.bg_color === 'string' && /^#[a-f0-9]{6}$/i.test(options.bg_color)){
+            bg_color = options.bg_color;
+         }
+
+         // Subtract border's stroke size from overall size 
+         let size = `${width - border_stroke * 2}x${height - border_stroke * 2}`;
+
+         
+         cmd.push(`${bin}`);
+         cmd.push(`-size ${size}`);
+         cmd.push(`-quality ${quality}`);
+         cmd.push(`-border ${border_stroke}`);
+         cmd.push(`-bordercolor ${border_color}`);
+         cmd.push(`xc:${bg_color}`);
+         cmd.push(`${path.normalize(output_path)}`);
+         await exec(cmd.join(' '));
+
+         failover_path = output_path;
+
+      } catch(err) {
+         console.log(err.message);
+      } finally {
+         resolve(failover_path);
+      }
+   })
+}
+
+module.exports = makeFailover;
